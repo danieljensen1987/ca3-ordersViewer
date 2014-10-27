2901db6e2bc7ce506a34aff43869cbc39c63cb63
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-10-27 14:42:20 +0100

    orders sortet when pulled

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var model = require('../database/model');
+var modelMongo = require('../database/modelMongo');
+
+function comparator(a, b) {
+    return new Date(b.orderDate).getTime() - new Date(a.orderDate).getTime();
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'MongoDB Viewer' });
+    modelMongo.connect();
+    model.OrderModel.find({}, function (err, orders) {
+        if (err){
+            res.render('index', { orders: err, title: 'Orders' });
+        } else {
+            res.render('index', { orders: orders.sort(comparator), title: 'Orders' });
+        }
+        modelMongo.close();
+    })
 });
 
 module.exports = router;
