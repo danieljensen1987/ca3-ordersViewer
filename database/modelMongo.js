b3dd9c87aba14f8b00fa04e8392b3300bc2a5907
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-10-27 14:51:19 +0100

    file added to handle open and close connection to db

--- database/modelMongo.js
+++ database/modelMongo.js
@@ -1,3 +1,24 @@
-/**
- * Created by Daniel on 27-10-2014.
- */
+var mongoose = require('mongoose');
+//var dbURI = 'mongodb://localhost:27017';
+var dbURI = 'mongodb://test:test@ds049180.mongolab.com:49180/ordersviewer';
+
+module.exports.connect = function(){
+//    mongoose.connect("mongodb://localhost/test");
+    mongoose.connect(dbURI);
+
+    var db = mongoose.connection;
+    db.once('open', function(){
+//        console.log('Connected ' + db.port);
+        console.log('Connected ' + dbURI);
+    });
+
+    db.on('error', function (err) {
+        console.log(err);
+    });
+
+};
+
+module.exports.close = function(){
+    mongoose.connection.close();
+    console.log('Connection closed');
+};
