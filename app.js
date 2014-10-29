a7fd028339f0c4c163ffdab65e27bd02b8609e07
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-10-29 13:19:03 +0100

    Minor code clean-up. Menu modified

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var categories = require('./routes/categories');
+var products = require('./routes/products');
 var users = require('./routes/users');
 
 var app = express();
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/categories', categories);
+app.use('/products', products);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
